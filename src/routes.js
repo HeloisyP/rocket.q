6df0690361624836e9a13d27e9b72579eff8950d
Heloisy Pimenta Queiroz <heloisy.pimenta1@gmail.com>
2021-11-07 20:16:08 -0400

    Requisições em andamentos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const questionController = require('./controllers/questionController')
 
 const routes = express.Router()
 
@@ -7,5 +8,8 @@ routes.get('/room', (req, res) => res.render("room"))
 routes.get('/create-pass', (req, res) => res.render("create-pass"))
 
 routes.get('/room/:room/:question/:action', (req, res) => res.render("exemplo", {req}))
+
+// Formato do dados capturados na modal
+routes.post('/room/:room/:question/:action', questionController.index)
 
 module.exports = routes
